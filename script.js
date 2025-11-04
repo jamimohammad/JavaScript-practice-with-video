05503c4f6fc8e7db20fab239d79ef123ffd957fd
Mohammad <jamim1382@gmail.com>
2025-11-04 14:53:18 +0430

    First commit

---
+++ script.js
@@ -0,0 +1,631 @@
+
+//data types
+//five
+// var user = 7676;
+// alert(typeof user);
+// var name = 'mohammad'
+// var bo = true
+///////////////////
+//seven and eight
+//prompt and convert data types
+// var userage = prompt ("please inter your name","hello how are you")
+// //  var usernumber = number (userage)// we can change number to the string
+//  var usernumber = +userage// if we want to change the data types we add a plus
+// alert (typeof usernumber)
+// //string
+// var age = 23
+// var change = String(age)
+// alert (typeof age)
+// all of number will be true به غیر zero
+// همه string ها true هستند به غیر از string خالی if there is a space will be true
+//////////////////////////
+//logical opertators
+//nine
+// let firstnumber = 20
+// let secondnuber = 40
+// let plus = firstnumber + secondnuber
+// console.log (firstnumber + secondnuber);
+// console.log (plus)
+// let tagsim = firstnumber / secondnuber
+// console.log(tagsim)
+// let zarb = firstnumber * secondnuber
+// console.log (zarb)
+// let tavan = firstnumber ** secondnuber
+// console.log (tavan)
+// let bagi = 18 % 5
+// console.log (bagi)
+///////////////
+//slove
+//ten
+// let ali = 'heloo'
+// let ahmad = 'hi'
+// let number1 =prompt ("please inter your name")
+// let number2 =prompt ("please  second  name")
+// let number1type = number(number1)
+// let number2type = number(number2)
+// let bo = number1type + number2type
+// alert (bo)
+/////////////////////
+//concat
+//eleven
+// let contact = 'hello how are you'
+// let cancat = 'java is fun'
+// let both = 'site:' + contact + "\+" + cancat
+// alert(both)
+// alert( 'site: ' + contact + "\n" + cancat)
+// alert ("hello \"hozair\" shah" + "what are you doing")
+/////////////////////
+//twelve
+//nan
+// alert("12s" / 4)
+// let string = 'hello java is fun'
+// let number = 12
+// alert (string + ' ' + number)
+///////////////
+
+//thirteen
+//method nan
+// let str1 = 'sanzlearn'
+// let srt2 = 'java is fun'
+// let num1 = 12
+// let num2 = 4
+// alert(isNaN(num2 / num1))
+/////////////////////////
+//fourteen
+//comparative-operators
+// let num1 = 12
+// let num2 = 4
+// let num3 = 3
+// let str1 = "3"
+// let str2 = "4"
+// alert(num1 == num3)
+// alert (num2 * num3 == num1)
+// alert(num1 / num3 == num2)
+// alert(num1 > num2)
+// alert (num2 * num3 > num1)
+// alert (str1 == num3)
+// alert (str1 === num3)
+/////////////////////
+//fifteen
+//use if
+// let num1 = 12
+// let num2 = 3
+// let num3 = 4
+// let num4 = "12"
+// if (num2 + num3 == num1){
+//     alert("hi")
+// }else{
+//     alert ('no it is not good')
+// }
+////////////////////
+//sixteen
+//solove
+// let user = prompt("inter your name:")
+// if (user % 4 == 0){
+//     alert("عدد زوج میباشد ")
+// }else{
+//     alert("هدد ")
+// }
+//////////////
+//seventeen
+//average
+// let firstnumber = Number(prompt ("enter your first number"))
+// let secondnumber =Number(prompt ("enter your second number"))
+// let thirdnumber = Number(prompt ("enter your third number"))
+// let all = (firstnumber + secondnumber + thirdnumber) / 3
+// console.log (all)
+/////////////////
+//eighteen
+//solove
+// let age = prompt("inter your age")
+
+// if(age> 18){
+//     alert("your age is good")
+// }else{
+//     alert("your age is not good")
+// }
+///////////////
+//nineteen
+//solove calculation
+// let number = prompt("inter first number")
+// let number1 = prompt("inter second number")
+
+// let both = number ** number1
+// alert (both)
+////////////////////
+//بیست
+// solove user age
+// let age = prompt("inter your age")
+
+// if (isNaN(age)){
+//     alert ("تاریخ تولد معتبر نیستگ")
+// }
+// let her = 1404 - age
+// alert(her)
+////////////////////
+//بیست یک
+//convert minuts to hours
+// let tiem = prompt("inter your time")
+
+// let minuts = tiem / 60
+// alert(minuts)
+// if (isNaN(minuts)){
+//     alert("دد وارد شده معتبر نیست")
+// }
+//بیست و دو
+//if and else
+// let num1 = 12
+//  let num2 = 4
+//   let num3 = 3
+
+//   if (num3 * num2 === 10){
+//     alert("good")
+//   }else if (num3 * num2 === 12){
+//     alert
+//      ("this is good")
+//   }else {
+//     alert("all are not good")
+//   }
+//بیست و سه
+//bit-operator-or
+//     let num1 = 12
+//  let num2 = 4
+//   let num3 = 3
+
+//   if (num3 * num2 === 10 || num3 * num2 === 12){
+//     alert("good")
+//   }
+
+// بیست و چهار
+//slove level practice
+// let age = prompt("enter your age")
+// let gender = prompt("enter your gender (male, female)")
+
+// if (gender === 'female' || age < 18 ){
+//     alert("you are not good")
+// }else {
+//     alert (" your age is good")
+// }
+////////////////
+//بیست پنج
+//and operators
+//  let num2 = 4
+//   let num3 = 3
+//   if (num2 * 3 && num2 * num3 === 12){
+//     alert("good")
+//   }else if (num2 * 4 && num2 * num3 === 15){
+//     alert("hi")
+//   }
+//     else{
+//         alert('hello')
+//     }
+//     //nagative
+// let bool = true
+//  alert(!bool)
+// alert(!!bool)
+///////////////////
+//بیست و شیش
+//ternary condition خلاصه نوشتن
+//   let num2 = 4
+//   let num3 = 3
+//    num2 * num3 === 12 ?  alert('hello') : alert("hi:")
+////////////////////
+//بیست و هفت
+//switch case
+// switch (5){
+//     case 1:  
+// alert('satarday')
+//     break;
+//     case 2:
+//         alert('sunday')
+//         break;
+//      case 3:
+//         alert('monday')
+//     break;
+//      case 4:
+//         alert('tusday')
+//     break;
+//      case 5:
+//         alert('tharthday')
+//     break;
+//     default:
+//         alert('good')
+//         break;
+// }
+////////////////////////////
+// بیست و هشت
+//Solve-the-leveling-exercise-with-the-help-of-Switch-Case
+// let slove = prompt(Number("inter your score please"))
+// switch (slove) {
+//     case "18":
+//     case "19":
+//     case "20":
+//         alert("a")
+//         break
+//     case "15":
+//     case "16":
+//     case "17":
+//         alert("b")
+//         break
+//     case "12":
+//     case '13':
+//     case "14":
+//         alert("c")
+//         break
+// default:
+//     alert("شما مشروط هستید")
+// }
+///////////////////////
+//بیست و نه
+//Work-with-declaration-functions
+// function showvalue (){
+//     var num1 = 4
+//     var num2 = 8
+//     var result = num1 + num2
+//     alert(result)
+// }
+// showvalue()
+//////////////////////////////
+//سی
+//30-Work-with-expressions-functions
+// var showvalue = function  (){
+//     var num1 = 4
+//     var num2 = 8
+//     var result = num1 + num2
+//     alert(result)
+// }
+// showvalue() 
+////////////////////////////
+
+
+// 31-Manage-function-parameters
+// سی و یک
+// function showvalue (hello){
+
+//     alert('hi' + hello)
+// }
+// showvalue('how are you') 
+// function showvalue (FileReader, hello){
+//     alert('FileReader' + FileReader)
+//      alert('hello' + hello)
+// }
+// showvalue(34, 17) 
+///////////////////////
+
+
+//سی و دو
+//32-Solve-even-or-odd-input-number-exercise-with-help-of-function
+// function text (number){
+// if (number % 2 === 0){
+//     alert(number + 'it is even')
+// }else{
+//     alert(number + 'it is odd')
+// }
+// }
+// text(5)
+// text(10)
+// text(100)
+// text(1995)
+//سی و سه
+//33-Solve-exercise-calculating-average-of-input-numbers-using-function
+// function check (num1, num2, num3){
+// var three = num1 + num2 + num3
+// var all = three / 3
+// alert(all)
+// }
+
+// check(150, 100, 11)
+//سی و چهار
+//34-Determine-default-values-for-the-input-of-functions
+// function check (num1, num2){
+// var three = num1 + num2
+// // alert(three)
+// return three
+// }
+// var tes = check(150, 100, 11)
+// alert(tes)
+///////////////////////
+// var number1 = prompt("enter a number")
+// var number2 = prompt("enter a number")
+
+// function show (num1, num2){
+// return num1 ** num2
+// }
+// var all =show(number1, number2)
+// alert(all)
+
+//سی و پنج
+//35-role-of-the-return-keyword-in-functions
+//  var number1 = prompt("enter a number")
+// var number2 = prompt("enter a number")
+//  function show (num1){
+//     alert(num1, num2)
+// return num1 ** num2
+// }
+// var all =show(number1, number2)
+// alert(all)
+
+
+//سی و شیش
+//36-String-structure-and-properties
+// var test = 'My name is mohammad'
+// var other = test [0]
+// alert(other)
+// alert(test[6])
+// alert(test.length)
+
+
+//سی و هفت
+//37-Solve-Register-exercise
+// var username = prompt('write your name')
+// var userpassword = prompt('write your password')
+
+// if (username.length < 3 || userpassword.length < 8){
+//     alert('bad')
+// }
+// else{
+//     alert('good')
+// }
+
+//سی و هشت
+//38-string-methods-1
+// var like = 'Ai love java script'
+// console.log (like.charAt(0))
+// console.log (like.charCodeAt(0))//ascii
+// console.log (like.concat ('and reacr'))
+// var final = (like.concat ('and reacr'))
+// console.log (final)
+// console.log (like.trim()) // space ها را حذف م
+// console.log(like.toLocaleLowerCase())
+// console.log(like.toUpperCase())
+// console.log(like.search('java'))
+// console.log(like.indexOf('java'))
+// console.log(like.includes('java'))
+
+
+//سی و نه
+//39-Solve-login-exercise-as-Case-Insensiti
+//  var user = prompt('enter user name')
+//  if (user.toLowerCase() == 'ali'){
+// alert('good')
+//  }else{
+// alert('bad')
+//  }
+
+
+//چهل
+//40-string-methods-2
+//   var like = 'i love javascript & react'
+//   console.log(like.slice(7, 17))
+// console.log(like.substr(7, 10)) // lenght را میخواد
+// console.log(like.substring(7, 10))  
+
+
+
+//چهل و یک
+//41-Working-with-Math-meth
+// console.log (Math.PI)
+// console.log (Math.pow(2, 4 ))
+//  console.log (Math.sqrt(16))
+//   console.log (Math.abs(-16))
+//  console.log (Math.min(16, 74, 48, 484))
+//  console.log (Math.max(16, 74, 48, 484))
+//  console.log (Math.floor(16.3))// قسمت اعشاری را خزف میگند
+//  console.log (Math.trunc(16.2)) like math floor
+//  console.log (Math.round(16.8))
+//  console.log (Math.ceil(15.6)) به هر کدام که نزدیک باشه اون را نمایش میده
+//  console.log (Math.random() * 10) کلمه رندوم را چاپ میکنه
+
+
+//چهل و دو
+//42-Solve-random-captcha-code-generation-exe
+// var random = Math.random() * 100000
+// var first = Math.floor(random)
+// console.log(first)
+
+
+//چهل و سه
+//43-Assignment-operators-in-JS
+// var number = 10
+// number = number + 2
+// number++
+// ++number
+// number += 2
+// console.log (number)
+
+
+//چهل و چهار
+//44-Difference-between-(++x)-and-(x++)
+// var number1 = 2
+// var number2 = number1++
+// console.log (number1)
+// console.log(number2)
+
+
+
+//چهل و پنج
+//45-How-to-work-with-the-repeat-loop-for
+// for (let a = 0 ; a < 10 ; a++ ){
+//     alert(a)
+// }
+
+
+//چهل و شیش
+//46-for-Loop-Supplementary-Tips
+// for (let a = 3 ; a > 0 ; a-- ){
+//     alert(a)
+// }
+// var first = 'mohammad'
+// for(let a = 0 ; a < first.length ; a++){
+//     alert(first[a])
+// }
+
+
+//چهل و هفت
+//47-Solve-exercise-displaying-even-numbers-between-0__100-with-for-loop
+// for (var i = 0 ; i < 100 ; i += 2){
+// console.log (i)
+// }
+
+
+//چهل و هشت
+//48-How-to-trace-code-for-better-understanding
+// for (var i = 0 ; i < 100 ; i += 2){
+// console.log (i)
+// }
+
+
+//چهل و نه
+//49-Solve-Exercise-Shopping-Cart-Price-with-for-loop
+// var all = 0
+// for (var check = 0 ; check < 5 ; check++){
+// all = all + Number(prompt('enter the price'))
+// }
+// alert(all)
+
+
+//پنجاه
+//50-Solve-average-calculation-exercise-with-for-loop
+// var zero = null
+// var user = 0
+// for (var a = 0; a < 5 ; a++){
+// user = Number(prompt('enter a number'))
+
+// zero = zero + user
+// }
+// console.log(zero / 5)
+
+
+
+//پنجاه و یک
+//51-Solve-exercise-of-calculating-digits-of-input-number
+
+// var user = Number(prompt('enter a number'))
+// var sum = 0
+// for (var i = 0; (user / 10) != 0; i++) {
+//     sum = sum + (user % 10)
+//     user = Math.floor(user / 10)
+// }
+// alert(sum)
+
+//پنجاه و دو
+//52-Solve-exercise-of-number-of-digits-of-input-number-with-for-loop
+// var usernumber = Number(prompt('enter a number'))
+// var counter = 0
+// for (var i = 0 ; usernumber / 10 != 0 ; i++){
+//     counter++
+//      usernumber = Math.floor (usernumber / 10)
+// }
+// alert(counter)
+
+
+//پنجاه و  سه
+//53-How-to-work-with-while-loops+solve-exercises.mp4
+// var i = 0 
+// while (i < 10){
+// alert(i)
+// i += 2
+// }
+
+// var usernumber
+// var i = 0
+// var sum = 0
+// while(i < 3){
+// usernumber = Number(prompt('enter a number'))
+// sum = sum + usernumber
+// i++
+// }
+// alert(sum / 3)
+
+
+// پنجاه و چهار
+//54-Solve-exercises-with-while-loops.mp4
+
+// var user = Number(prompt('enter a number'))
+// var sum = 0
+// var counter = 0
+// while(user / 10  != 0){
+//     sum = sum + (user % 10)
+//     counter++
+//     user = Math.floor(user / 10)
+// }
+// console.log(sum , counter)
+
+//پنجاه و پنج
+//55-Solve-exercise-displaying-even-numbers-between-input-numbers-with-while-loop.mp4
+
+// var user = Number(prompt('Enter a number'))
+// var user1 = Number(prompt('Enter a number'))
+
+// if (user % 2 === 0){
+// while(user < user1){
+//     console.log(user)
+//     user += 2 
+// }
+// }else {
+//     user++
+//     while(user < user1){
+//     console.log(user)
+//     user += 2 
+//     }
+// }
+
+
+// پنجاه و شیش
+//56-Solve-exercise-of-calculating-average-of-numbers-to-users-desired-number-(with-while-loop.mp4
+// var user = 0
+// var sum = 0
+// var count = 0
+// while (user != -1) {
+//   sum = sum + user
+//   user = Number(prompt('Enter a number'))
+//   if (user != -1){
+//  count++
+//   }
+// }
+// console.log(sum, count)
+
+// پنجاه و هفت
+//57-How-to-work-with-do-while-loop.mp4
+// var i = 0
+// do{
+// console.log(i)
+// i++
+// }while(i < 5)
+
+
+// پنجاه و هشت
+//58-Solve-power-training-with-three-loops-for-while-doWhile.mp4
+/////////////for///////////////
+// var user1 = Number(prompt('enter a number'))
+// var user2 = Number(prompt('enter a number'))
+
+// var power =1
+// for (let i = 0; i < user2; i++){
+//     power = power * user1
+// }
+// alert('power' + power) 
+////////////while////////////
+// var power =1
+// var user1 = Number(prompt('enter a number'))
+// var user2 = Number(prompt('enter a number'))
+
+// var i =0
+// while (i < user2){
+//     power = power * user1
+//     i++
+// }
+// alert('power' + power) 
+/////////////do while///////////////
+// var user1 = Number(prompt('enter a number'))
+// var user2 = Number(prompt('enter a number'))
+
+// var power =1
+// var i =0
+// do{
+// power = power * user1
+//  i++
+// }
+// while (i < user2){
+// }   
+// alert('power' + power) 
